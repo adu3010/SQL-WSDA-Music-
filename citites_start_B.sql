88ed754ed20e29fa32e0cba1e5dc3ac7aed213ed
Aditya Gadkari <50283146+adu3010@users.noreply.github.com>
2024-06-12 00:31:22 +0530

    citites_start_B.sql

--- citites_start_B.sql
+++ citites_start_B.sql
@@ -1,9 +1,3 @@
-/*CREATED BY: SAM AGUIAR
-CREATE DATE: 09/19/2023
-DESCRIPTION: How many invoices were billed in citities that start with B?
-*/
--- % I don't care what comes next
-
 SELECT
 	InvoiceDate,
 	BillingAddress,
