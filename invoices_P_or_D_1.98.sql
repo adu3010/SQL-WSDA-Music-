b2e56434a0e1ddf5b36276e58bb0f03bd64b8077
Aditya Gadkari <50283146+adu3010@users.noreply.github.com>
2024-06-12 00:36:46 +0530

    invoices_P_or_D_1.98.sql

--- invoices_P_or_D_1.98.sql
+++ invoices_P_or_D_1.98.sql
@@ -1,9 +1,3 @@
-/*CREATED BY: SAM AGUIAR
-CREATE DATE: 09/19/2023
-DESCRIPTION: Get a list of all invoices who's billing city starts with P or D and whose total is greater than 1.98.
-*/
--- % I don't care what comes next
-
 SELECT
 	InvoiceDate,
 	BillingAddress,
