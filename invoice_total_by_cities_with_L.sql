7275366804b117d74c7319e99670a9c927821051
Aditya Gadkari <50283146+adu3010@users.noreply.github.com>
2024-06-12 00:35:44 +0530

    invoice_total_by_cities_with_L.sql

--- invoice_total_by_cities_with_L.sql
+++ invoice_total_by_cities_with_L.sql
@@ -1,9 +1,3 @@
-/*
-Created by: Sam Aguiar
-Create Date: 09/20/23
-Description: What are the average invoice total by city for only the cities that start with L?
-*/
-
 SELECT
 	BillingCity,
 	ROUND(avg(Total), 2)
