8871fe919d279d4ca75eb5312cbfc76bcb457874
Aditya Gadkari <50283146+adu3010@users.noreply.github.com>
2024-06-12 00:36:09 +0530

    invoice_total_less_3.96.sql

--- invoice_total_less_3.96.sql
+++ invoice_total_less_3.96.sql
@@ -1,9 +1,3 @@
-/*CREATED BY: SAM AGUIAR
-CREATE DATE: 09/19/2023
-DESCRIPTION: Get a list of all invoices billed after 2010-05-22 and have a total less than 3.00.
-*/
--- % I don't care what comes next
-
 SELECT
 	InvoiceDate,
 	BillingAddress,
