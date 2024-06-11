b81d66f7ed3453f69926c685322e48770b3e5cca
Aditya Gadkari <50283146+adu3010@users.noreply.github.com>
2024-06-12 00:34:23 +0530

    customer_names.sql

--- customer_names.sql
+++ customer_names.sql
@@ -1,9 +1,3 @@
-/*
-CREATED BY: SAM AGUIAR
-CREATE DATE: 09/19/2023
-DESCRIPTION: This query diesplays all customers first, last names, and emails
-*/
-
 SELECT
 	FirstName AS [Customer First Name],
 	LastName AS "Customer Last Name",
